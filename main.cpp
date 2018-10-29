f76a3f5d227b6a205b2fa9a95f40ee81dab4a674
connordoman <connor.doman@student.ufv.ca>
2018-10-29 13:57:37 -0700

    can count files char size

--- main.cpp
+++ main.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
-#include <ifstream>
-#include <ofstream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
-int main() {
-	cout << "Hello world!" << endl;
+long fileCharLength(ifstream &inStream) {
+	if (inStream.is_open()) {
+		string line;
+		long numChars = 0;
+
+		while (getline(inStream, line)){
+			numChars += line.length();
+		}
+		return numChars;
+	} else {
+		cerr << "File not opened." << endl;
+		return -1;
+	}
+}
+
+int main(int argc, char *argv[]) {
+	string *filelocation;
+	ifstream script_file;
+
+	*filelocation = argv[1];
+
+	script_file.open(*filelocation);
+	cout << fileCharLength(script_file) << endl;
+	script_file.close();
 
 	return 0;
 }
